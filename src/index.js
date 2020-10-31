6d07024e3862eff922a0c2548b541fe597b8e079
deepak bhatt <deepakbhattmits@gmail.com>
2020-10-31 09:02:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './assets/css/styles.css';
 
-// rootElement
+//rootElement
 const rootElement = document.querySelector('#root');
 ReactDOM.render(<App />, rootElement);
