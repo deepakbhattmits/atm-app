47197be782e480c43c4098b117478a5ed60dab22
deepakbhattmits <deepakbhattmits@gmail.com>
2021-09-27 07:26:34 +0530

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+/** @format */
+
+// import React from 'react';
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import './assets/css/_styles.scss'
+
+//rootElement
+const rootElement = document.querySelector('#root')
+ReactDOM.render(<App />, rootElement)
